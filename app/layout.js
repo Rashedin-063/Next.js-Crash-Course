d51c3997369bf9b6f31d79966c94fdf7a4c01f95
RU063 <rashedinislam.06@gmail.com>
2024-10-14 23:28:31 +0600

    about page added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,10 +20,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="mx-4 bg-blue-950 ">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav className='py-4 bg-blue-950 opacity-45 text-white font-semibold flex gap-6 justify-center'>
+          <Link href='/'>Home</Link>
+          <Link href='/about'>About</Link>
+       </nav>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -7,9 +7,9 @@ export default function Home() {
           <h1 className="text-3xl text-yellow-600 font-semibold italic"> Hello World </h1>
       <p className="block italic mt-2 text-green-600">I am rashedin islam, a junior mern stack developer
         </p>
-        <p className="mt-2 text-sm">Gosh! It's too slow!!! what else can I do now! I need to upgrade my laptop right now. Why it is this slow! I'm so disappointed. I'm helpless here</p>
+        <p className="mt-2 text-sm">Gosh! It&apos;s too slow!!! what else can I do now! I need to upgrade my laptop right now. Why it is this slow! I&apos;m so disappointed. I&apos;m helpless here</p>
         <button></button>
-        <button className="bg-green-900 px-6 py-2 mt-2 rounded-full text-sm text-yellow-sunshine">Help Me</button>
+        <button className="bg-green-900 px-6 py-2 mt-2 rounded-full text-sm text-yellow-600">Help Me</button>
       </div>
    </div>
   );
